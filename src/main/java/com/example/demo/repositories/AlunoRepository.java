10c6ffd875d7bc1e887a27bed7eaaee8c68d70ab
LucasJuszczak <161529868+LucasJuszczak@users.noreply.github.com>
2024-12-03 20:45:29 -0300

    Update

--- src/main/java/com/example/demo/repositories/AlunoRepository.java
+++ src/main/java/com/example/demo/repositories/AlunoRepository.java
@@ -9,4 +9,5 @@ import java.util.List;
 @Repository
 public interface AlunoRepository extends CrudRepository<Aluno, Integer> {
     List<Aluno> findByNome(String nome);
+    List<Aluno> findByEmail(String email);
 }
