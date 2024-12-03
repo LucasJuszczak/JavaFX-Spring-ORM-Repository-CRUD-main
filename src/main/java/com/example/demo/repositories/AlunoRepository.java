8ce4126ff9b0ed7a113bf502e03f404a4a2083a8
LucasJuszczak <161529868+LucasJuszczak@users.noreply.github.com>
2024-12-03 20:10:50 -0300

    Update AlunoRepository.java

--- src/main/java/com/example/demo/repositories/AlunoRepository.java
+++ src/main/java/com/example/demo/repositories/AlunoRepository.java
@@ -4,5 +4,9 @@ import com.example.demo.entities.Aluno;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface AlunoRepository extends CrudRepository<Aluno, Integer> { }
+public interface AlunoRepository extends CrudRepository<Aluno, Integer> {
+    List<Aluno> findByNome(String nome);
+}
